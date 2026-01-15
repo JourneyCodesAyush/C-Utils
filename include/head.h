f151b73251cbe22c145323e6a0a5364df26fe6ee
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-01-15 16:54:30 +0530

    feat(command): add 'head' command
    - Prints the first n lines of a file

---
+++ include/head.h
@@ -0,0 +1,7 @@
+#ifndef C_UTILS_HEAD_H
+#define C_UTILS_HEAD_H
+
+// Print the first 'number_of_lines' lines of 'filename'
+void command_head(const char *filename, int number_of_lines);
+
+#endif // C_UTILS_HEAD_H
---
+++ src/head.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "common.h"
+#include "head.h"
+
+// Print the first 'number_of_lines' lines of a file
+void command_head(const char *filename, int number_of_lines)
+{
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+    {
+        fprintf(stderr, "Couldn't open file %s\n", filename);
+        exit(1);
+    }
+
+    char line[MAX_CHARS_IN_LINE];
+
+    for (int i = 0; i < number_of_lines; i++)
+    {
+        if (!fgets(line, MAX_CHARS_IN_LINE, fp))
+            break;
+        fprintf(stdout, "%s", line);
+    }
+
+    fclose(fp);
+}
