3b253db23fa43237b63be59faeb57fe4fcb4075c
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-01-15 16:48:23 +0530

    feat(common): add header to include commonly used variables and constants

---
+++ include/common.h
@@ -0,0 +1,8 @@
+#ifndef C_UTILS_COMMON_H
+#define C_UTILS_COMMON_H
+
+// Maximum characters in a single line for head/tail/wc buffers
+// Note: Lines longer than this may be truncated
+#define MAX_CHARS_IN_LINE 1024
+
+#endif // C_UTILS_COMMON_H
