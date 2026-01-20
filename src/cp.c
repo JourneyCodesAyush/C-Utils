4a35d22a13b8ce91ad2e8b0f71bfec4287dd956b
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-01-20 13:37:39 +0530

    refactor(commands): copy files using block I/O for efficiency and speed
    - Use helper function to close files
    - Copy file using block I/O for better speed and cross platform support (line endings)

--- src/cp.c
+++ src/cp.c
@@ -3,19 +3,25 @@
 
 #include "cp.h"
 
+static void close_files(FILE *f1, FILE *f2)
+{
+    if (f1)
+        fclose(f1);
+    if (f2)
+        fclose(f2);
+}
+
 void command_cp(const char *source, const char *destination)
 {
-    FILE *f1 = fopen(source, "r");
-    FILE *f2 = fopen(destination, "w");
+    FILE *f1 = fopen(source, "rb");
 
     if (f1 == NULL)
     {
         fprintf(stderr, "Couldn't open file: %s\n", source);
-        if (f2 != NULL)
-            fclose(f2);
-
         exit(EXIT_FAILURE);
     }
+
+    FILE *f2 = fopen(destination, "wb");
 
     if (f2 == NULL)
     {
@@ -26,31 +32,37 @@ void command_cp(const char *source, const char *destination)
         exit(EXIT_FAILURE);
     }
 
-    int ch;
-    while (1)
+    /* Use block I/O for efficiency while remaining portable */
+    char buffer[BUFSIZ];
+
+    size_t bytes_read;
+
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), f1)) > 0)
     {
-        ch = fgetc(f1);
-        if (ch == EOF)
-            break;
-        else
+        size_t bytes_written = 0;
+        while (bytes_written < bytes_read)
         {
-            int res;
-            res = fputc((char)ch, f2);
-            if (res == EOF)
+            size_t n = fwrite(buffer + bytes_written, 1, bytes_read - bytes_written, f2);
+            if (n == 0)
             {
-                fprintf(stderr, "Couldn't copy file %s to %s\n", source, destination);
-                fclose(f1);
-                fclose(f2);
+                fprintf(stderr, "Couldn't copy %s to %s\n", source, destination);
+                close_files(f1, f2);
                 exit(EXIT_FAILURE);
             }
+            bytes_written += n;
         }
     }
 
     if (ferror(f1))
     {
         fprintf(stderr, "Error reading file %s\n", source);
-        fclose(f1);
-        fclose(f2);
+        close_files(f1, f2);
+        exit(EXIT_FAILURE);
+    }
+    if (ferror(f2))
+    {
+        fprintf(stderr, "Error reading file %s\n", destination);
+        close_files(f1, f2);
         exit(EXIT_FAILURE);
     }
 
